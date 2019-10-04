c9f7f4cda9f8a81b8d22c77127d591b1970af202
Duckelekuuk <Duckelekuuk@users.noreply.github.com>
2019-10-04 17:39:48 +0200

    Update ChatManager.java

--- src/main/java/com/thejoeyl/ChatManager.java
+++ src/main/java/com/thejoeyl/ChatManager.java
@@ -10,30 +10,35 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class ChatManager extends JavaPlugin implements Listener {
 
     @EventHandler
-    public void badWords(AsyncPlayerChatEvent e) {
-        for (String word : e.getMessage().split(" ")) {
-            if (getConfig().getStringList("badwords").contains(word)) {
-                e.setCancelled(true);
-                e.getPlayer().sendMessage(ChatColor.RED + "DONT CURSE!");
-            }
+    public void badWords(AsyncPlayerChatEvent event) {
+        for (String word : event.getMessage().split(" ")) {
+            if (!getConfig().getStringList("badwords").contains(word)) continue;
+            
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(ChatColor.RED + "DONT CURSE!");
         }
     }
 
     public void onEnable() {
+        //Copy default config if not existing
         getConfig().options().copyDefaults(true);
         saveConfig();
-        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        
+        // Registering listeners
+        getServer().getPluginManager().registerEvents(this, this);
         getServer().getPluginManager().registerEvents(new ListenerToStaff(),this);
         getServer().getPluginManager().registerEvents(new ChatColorCheck(),this);
-        this.getCommand("warn").setExecutor(new Warn());
-        this.getCommand("warnings").setExecutor(new Warnings());
-        this.getCommand("warningsremove").setExecutor(new WarningsRemove());
-        this.getCommand("warningsclear").setExecutor(new WarningsClear());
-        this.getCommand("sthelp").setExecutor(new StaffHelp());
-        this.getCommand("hetisstil").setExecutor(new Memes());
-        this.getCommand("chatcolorblue").setExecutor(new ColorChat());
-        this.getCommand("chatcolorreset").setExecutor(new ColorChat());
-        this.getCommand("chatcolorred").setExecutor(new ColorChat());
-        this.getCommand("chatcoloryellow").setExecutor(new ColorChat());
+        
+        //Registering commands
+        getCommand("warn").setExecutor(new Warn());
+        getCommand("warnings").setExecutor(new Warnings());
+        getCommand("warningsremove").setExecutor(new WarningsRemove());
+        getCommand("warningsclear").setExecutor(new WarningsClear());
+        getCommand("sthelp").setExecutor(new StaffHelp());
+        getCommand("hetisstil").setExecutor(new Memes());
+        getCommand("chatcolorblue").setExecutor(new ColorChat());
+        getCommand("chatcolorreset").setExecutor(new ColorChat());
+        getCommand("chatcolorred").setExecutor(new ColorChat());
+        getCommand("chatcoloryellow").setExecutor(new ColorChat());
     }
 }
